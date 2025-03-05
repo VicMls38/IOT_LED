c08d0c8b68adecffaf53c2ebd8e512902c520dd9
Victor MORALES <victor.morales@outlook.fr>
2025-03-05 10:44:33 +0100

    Init commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: { origin: "*" } // Permet à l'ESP32 de se connecter
+});
+
+const port = 2002;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Routes pour chaque couleur
+app.get('/vert', (req, res) => {
+    io.emit('updateLED', 'vert');
+    res.send("LED Verte allumée !");
+});
+app.get('/jaune', (req, res) => {
+    io.emit('updateLED', 'jaune');
+    res.send("LED Jaune allumée !");
+});
+app.get('/rouge', (req, res) => {
+    io.emit('updateLED', 'rouge');
+    res.send("LED Rouge allumée !");
+});
+
+io.on('connection', (socket) => {
+    console.log('Un appareil est connecté');
+
+    socket.on('toggleLED', (state) => {
+        console.log(`Commande reçue pour LED : ${state}`);
+        io.emit('updateLED', state); // Diffuse l'état à tous les clients connectés
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Un appareil s\'est déconnecté');
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Serveur en écoute sur http://localhost:${port}`);
+});
